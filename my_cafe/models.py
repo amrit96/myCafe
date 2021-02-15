48f44c14031e82055c1d575a2126d9d28e850399
Amritayan <banerjee96.amritayan@gmail.com>
2021-02-15 11:49:06 +0530

    ABGN- Updated models

--- my_cafe/models.py
+++ my_cafe/models.py
@@ -6,22 +6,33 @@ class Profile(db.Model):
    name = db.Column(db.String, nullable=False)
    email = db.Column(db.String, nullable=False, unique=True)
    password = db.Column(db.String, nullable=False)
-
-   def __init__(self, name, email, password):
-      self.name = name
-      self.email = email
-      self.password = password
+   customer = db.Column(db.Boolean, nullable=False, default=True)
 
 
-class RawPurchase(db.Model):
-   id = db.Column('product_id', db.Integer, primary_key = True)
-   name = db.Column(db.String)
-   purchase_date = db.Column(db.DateTime, default=datetime.now)
-   expiry_date = db.Column(db.DateTime)
-   price = db.Column(db.Float)
+class Purchase(db.Model):
+   id = db.Column("transaction_id", db.Integer, primary_key = True)
+   purchase_date = db.Column(db.DateTime, nullable=False, default=datetime.now)
+   expiry_date = db.Column(db.DateTime, nullable=False)
+   price = db.Column(db.Float, nullable=False)
+   seller = db.Column(db.Integer, db.ForeignKey('supplier.id'), nullable=False)
+   product = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
 
-   def __init__(self, name, purchase_date, expiry_date,price):
-      self.name = name
-      self.purchase_date = purchase_date
-      self.expiry_date = expiry_date
-      self.price = price
+
+class Product(db.Model):
+   id = db.Column(db.Integer, primary_key=True)
+   name = db.Column(db.String, nullable=False)
+   category = db.Column(db.String, nullable=False)
+   availability_count = db.Column(db.Float, nullable=False)
+   unit = db.Column(db.String, nullable=False)
+   purchase = db.relationship('Purchase', backref='product', lazy=True)
+
+
+class Supplier(db.Model):
+   id = db.Column(db.Integer, primary_key=True)
+   first_name = db.Column(db.String, nullable=False)
+   middle_name = db.Column(db.String, nullable=True)
+   last_name = db.Column(db.String, nullable=True)
+   supply_type = db.Column(db.String, nullable=False)
+   contact = db.Column(db.Integer, nullable=False)
+   email = db.Column(db.String, nullable=True)
+   transaction = db.relationship('Purchase', backref='seller', lazy=True)
